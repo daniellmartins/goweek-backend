6f4b82fe3e49d129a2e50c39217e03e72dbc641c
Daniel Martins <daniellmarttins@hotmail.com>
2018-11-06 21:50:34 -0300

    add utils function getUserId

---
+++ src/utils/getUserId.js
@@ -0,0 +1,12 @@
+import { verify } from "jsonwebtoken";
+
+import { APP_SECRET } from "../config";
+
+export function getUserId(context) {
+  const Authorization = context.request.get("Authorization");
+  if (Authorization) {
+    const token = Authorization.replace("Bearer ", "");
+    const verifiedToken = verify(token, APP_SECRET);
+    return verifiedToken && verifiedToken.userId;
+  }
+}
